5584a88760bef3641fa2b3e8c2d6752334cd752a
gabriel valera <hashiramakage@gmail.com>
2022-12-03 15:20:09 -0400

    agregando menu movil funcional

--- frontend/components/dashboardComponent.jsx
+++ frontend/components/dashboardComponent.jsx
@@ -4,7 +4,7 @@ export default function DashboardComponent(props) {
     return (
         <div className="dashboard-component">
             <MenuComponent/>
-            {/* TODO: hacer el layout dashboard con tres columnas y trabjar el menu en fix para que se mantenga fijo en modo desktop */}
+            <div className="columna-oculta"></div>
             <section className="contenido">
                 {props.contenidoDasborad}
             </section>
