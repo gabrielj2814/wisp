db16812c572c126e93eeaf162a2ff29e78bda09d
gabriel valera <hashiramakage@gmail.com>
2022-12-27 17:46:15 -0400

    agregando el formulario al modulo de plan

--- frontend/pages/cliente/[operacion].js
+++ frontend/pages/cliente/[operacion].js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import DashboardComponent from '../../components/dashboardComponent'
-import {useRouter} from 'next/router'
+import { useRouter } from 'next/router'
 import BreadcrumbComponent from '../../components/breadcrumbComponent'
 import Link from "next/link"
 import AlertComponent from '../../components/alertComponent'
@@ -9,193 +9,193 @@ import CampoTextoNormalComponent from "../../components/campoTextoNormalComponen
 import CampoTextAreaNormalComponent from "../../components/campoTextAreaNormalComponent"
 import ModalComponent from '../../components/modalComponent'
 
-export default function formulario(props){
-  const router = useRouter()
-  let {operacion,id=null} = router.query
-  // migas de pan
-  const ruta= [
-    {
-      ruta:"/",
-      textLink:"Home"
-    },
-    {
-      ruta:"/cliente",
-      textLink:"Cliente"
-    },
-    {
-      textLink:operacion
-    }
-  ]
+export default function formulario(props) {
+    const router = useRouter()
+    let { operacion, id = null } = router.query
+    // migas de pan
+    const ruta = [
+        {
+            ruta: "/",
+            textLink: "Home"
+        },
+        {
+            ruta: "/cliente",
+            textLink: "Cliente"
+        },
+        {
+            textLink: operacion
+        }
+    ]
 
-  function modalToggle(boton){
-    let {target} = boton
-    modalTaggleId(target.getAttribute("data-id-modal"))
-  }
-
-  function modalTaggleId(id){
-    let modal = document.getElementById(id)
-    if(modal.classList.contains("show")){
-      modal.classList.remove("show")
-      modal.style.display="none"
-      modal.removeAttribute("role")
-      modal.removeAttribute("aria-modal")
-      modal.setAttribute("aria-hidden","true")
+    function modalToggle(boton) {
+        let { target } = boton
+        modalTaggleId(target.getAttribute("data-id-modal"))
     }
-    else{
-      modal.classList.add("show")
-      modal.style.display="block"
-      modal.setAttribute("role","dialog")
-      modal.setAttribute("aria-modal","true")
-      modal.removeAttribute("aria-hidden")
-    }
-  }
 
-  function cargarFoto(e){
-    let $imagen = document.querySelector(".imagen")
-    let {target} = e
-    let file = target.files[0]
-    console.log(file)
-    if(file){
-      const render = new FileReader()
-      render.addEventListener("load", (e) => {
-        let readerTarget = e.target
-        const img = document.createElement("img");
-        img.src = readerTarget.result;
-        img.classList.add("preview");
-        $imagen.textContent=""
-        $imagen.appendChild(img)
-      })
-      render.readAsDataURL(file)
+    function modalTaggleId(id) {
+        let modal = document.getElementById(id)
+        if (modal.classList.contains("show")) {
+            modal.classList.remove("show")
+            modal.style.display = "none"
+            modal.removeAttribute("role")
+            modal.removeAttribute("aria-modal")
+            modal.setAttribute("aria-hidden", "true")
+        }
+        else {
+            modal.classList.add("show")
+            modal.style.display = "block"
+            modal.setAttribute("role", "dialog")
+            modal.setAttribute("aria-modal", "true")
+            modal.removeAttribute("aria-hidden")
+        }
     }
-    else{
-      $imagen.textContent="Seleccionar Imagen"
+
+    function cargarFoto(e) {
+        let $imagen = document.querySelector(".imagen")
+        let { target } = e
+        let file = target.files[0]
+        console.log(file)
+        if (file) {
+            const render = new FileReader()
+            render.addEventListener("load", (e) => {
+                let readerTarget = e.target
+                const img = document.createElement("img");
+                img.src = readerTarget.result;
+                img.classList.add("preview");
+                $imagen.textContent = ""
+                $imagen.appendChild(img)
+            })
+            render.readAsDataURL(file)
+        }
+        else {
+            $imagen.textContent = "Seleccionar Imagen"
+        }
     }
-  }
 
-  function cancelarOnSubmit(e){
-    e.preventDefault()
-  }
-
-  function mostrarModal(){
-    modalTaggleId("modal")
-    let modalLabel = document.getElementById("modalLabel")
-    let mensajeModal = document.getElementById("mensajeModal")
-    if(operacion==="registrar"){
-      modalLabel.textContent="Registrar"
-      mensajeModal.textContent="Esta seguro que desea registrar"
+    function cancelarOnSubmit(e) {
+        e.preventDefault()
     }
-    else{
-      modalLabel.textContent="Actualizar"
-      mensajeModal.textContent="Esta seguro que desea actualizar"
+
+    function mostrarModal() {
+        modalTaggleId("modal")
+        let modalLabel = document.getElementById("modalLabel")
+        let mensajeModal = document.getElementById("mensajeModal")
+        if (operacion === "registrar") {
+            modalLabel.textContent = "Registrar"
+            mensajeModal.textContent = "Esta seguro que desea registrar"
+        }
+        else {
+            modalLabel.textContent = "Actualizar"
+            mensajeModal.textContent = "Esta seguro que desea actualizar"
+        }
     }
-  }
 
-  function enviarDatos(){
-    alert("ejecutando "+operacion)
-  }
+    function enviarDatos() {
+        alert("ejecutando " + operacion)
+    }
 
-  let contenido=(
-    <section className='min-vh-100 modulo-cliente-formulario'>
-      <BreadcrumbComponent ruta={ruta}/>
-      <div className='d-flex mb-5 px-5'>
-            <Link href="/cliente" className="btn btn-primary">Regresar</Link>
-      </div>
-      <h1 className=' text-white text-center mb-2'>Formulario Cliente</h1>
-      <div className='text-white text-center mb-4'>los campos con <span className='text-danger'>(*)</span> son obligatorios</div>
-      <div className=' container-fluid'>
-        <div className=' row justify-content-center'>
-          <div id="columnaAlerta" className=' col-10 col-sm-10 col-md-9 col-lg-9 col-xl-8'>
-            {/* zona aleta */}
-          </div>
-        </div>
-      </div>
-      <ModalComponent id="modal">
-            <div className="modal-header">
-              <h1 className="modal-title fs-5" id="modalLabel">---</h1>
-              <button type="button" className="btn-close" aria-label="Close" data-id-modal="modal" onClick={modalToggle}></button>
+    let contenido = (
+        <section className='min-vh-100 modulo-cliente-formulario'>
+            <BreadcrumbComponent ruta={ruta} />
+            <div className='d-flex mb-5 px-5'>
+                <Link href="/cliente" className="btn btn-primary">Regresar</Link>
             </div>
-            <div className="modal-body">
-              <h5 id='mensajeModal'></h5>
+            <h1 className=' text-white text-center mb-2'>Formulario Cliente</h1>
+            <div className='text-white text-center mb-4'>los campos con <span className='text-danger'>(*)</span> son obligatorios</div>
+            <div className=' container-fluid'>
+                <div className=' row justify-content-center'>
+                    <div id="columnaAlerta" className=' col-10 col-sm-10 col-md-9 col-lg-9 col-xl-8'>
+                        {/* zona aleta */}
+                    </div>
+                </div>
             </div>
-            <div className="modal-footer">
-              <button type="button" className="btn btn-secondary" data-id-modal="modal" onClick={modalToggle}>Cerrar</button>
-              {operacion==="registrar" && 
-                    <button onClick={enviarDatos} className="btn btn-primary">guardar</button>
-              }
-              {operacion==="actualizar" && 
-                <button onClick={enviarDatos} className="btn btn-warning">guardar</button>
-              }
+            <ModalComponent id="modal">
+                <div className="modal-header">
+                    <h1 className="modal-title fs-5" id="modalLabel">---</h1>
+                    <button type="button" className="btn-close" aria-label="Close" data-id-modal="modal" onClick={modalToggle}></button>
+                </div>
+                <div className="modal-body">
+                    <h5 id='mensajeModal'></h5>
+                </div>
+                <div className="modal-footer">
+                    <button type="button" className="btn btn-secondary" data-id-modal="modal" onClick={modalToggle}>Cerrar</button>
+                    {operacion === "registrar" &&
+                        <button onClick={enviarDatos} className="btn btn-primary">guardar</button>
+                    }
+                    {operacion === "actualizar" &&
+                        <button onClick={enviarDatos} className="btn btn-warning">guardar</button>
+                    }
+                </div>
+            </ModalComponent>
+            <div className=' container-fluid'>
+                <form id="formularioCliente" onSubmit={cancelarOnSubmit} className=' row justify-content-center'>
+                    <div className='col-12 col-sm-12 col-md-12 col-lg-11 col-xl-11'>
+                        <div className=' row justify-content-center'>
+                            <div className='mb-4 col-10 col-sm-10 col-md-3 col-lg-3 col-xl-3'>
+                                <CampoTextoNormalComponent id="cedula" name="cedula" campoObligatorio="1" labelCampo="Cedula" clasesStyleLabel="text-white" clasesStyleCampo="form-control" placeholder="CEDULA" />
+                            </div>
+                            <div className='mb-4 col-10 col-sm-10 col-md-3 col-lg-3 col-xl-3'>
+                                <CampoTextoNormalComponent id="nombres" name="nombres" campoObligatorio="1" labelCampo="Nombres" clasesStyleLabel="text-white" clasesStyleCampo="form-control" placeholder="NOMBRE" />
+                            </div>
+                            <div className='mb-4 col-10 col-sm-10 col-md-3 col-lg-3 col-xl-3'>
+                                <CampoTextoNormalComponent id="apellidos" name="apellidos" campoObligatorio="1" labelCampo="Apellidos" clasesStyleLabel="text-white" clasesStyleCampo="form-control" placeholder="APELLIDOS" />
+                            </div>
+                        </div>
+                        <div className=' row justify-content-center'>
+                            <div className='mb-4 col-10 col-sm-10 col-md-3 col-lg-3 col-xl-3'>
+                                <CampoTextoNormalComponent id="correo" name="correo" campoObligatorio="1" labelCampo="Correo" clasesStyleLabel="text-white" clasesStyleCampo="form-control" placeholder="ejemplo@gmail.com" />
+                            </div>
+                            <div className='mb-4 col-10 col-sm-10 col-md-3 col-lg-3 col-xl-3'>
+                                <CampoTextoNormalComponent id="telefono_movil" name="telefono_movil" campoObligatorio="1" labelCampo="Telefono Movil" clasesStyleLabel="text-white" clasesStyleCampo="form-control" placeholder="00000000000" />
+                            </div>
+                            <div className='mb-4 col-10 col-sm-10 col-md-3 col-lg-3 col-xl-3'>
+                                <CampoTextoNormalComponent id="telefono_fijo" name="telefono_fijo" campoObligatorio="1" labelCampo="Telefono Fijo" clasesStyleLabel="text-white" clasesStyleCampo="form-control" placeholder="00000000000" />
+                            </div>
+                        </div>
+                        <div className=' row justify-content-center'>
+                            <div className='mb-4 col-10 col-sm-10 col-md-9 col-lg-9 col-xl-9'>
+                                <CampoTextAreaNormalComponent id="direccion" name="direccion" campoObligatorio="1" labelCampo="Dirección" clasesStyleLabel="text-white" clasesStyleCampo="form-control" />
+                            </div>
+                        </div>
+                        <div className=' row justify-content-center mb-4'>
+                            <div className='col-10 col-sm-10 col-md-9 col-lg-9 col-xl-9'>
+                                <h2 className=' text-white'>Foto</h2>
+                            </div>
+                        </div>
+                        <div className=' row justify-content-center mb-5'>
+                            <input type="file" id='campoFoto' name='campoFoto' onChange={cargarFoto} hidden />
+                            <div className='col-auto'>
+                                <label htmlFor='campoFoto' className='imagen_label'>
+                                    <span className='imagen'>Seleccionar Imagen</span>
+                                </label>
+                            </div>
+                        </div>
+                        <div className=' row justify-content-center mb-4 '>
+                            <div className='col-auto'>
+                                {operacion === "registrar" &&
+                                    <button onClick={mostrarModal} className="btn btn-primary">Registra</button>
+                                }
+                                {operacion === "actualizar" &&
+                                    <button onClick={mostrarModal} className="btn btn-warning">Actualizar</button>
+                                }
+                            </div>
+                            <div className='col-auto'>
+                                <Link href="/cliente" className="btn btn-danger ">Cancelar</Link>
+                            </div>
+                        </div>
+                    </div>
+                </form>
             </div>
-          </ModalComponent>
-      <div className=' container-fluid'>
-        <form id="formularioCliente" onSubmit={cancelarOnSubmit} className=' row justify-content-center'>
-          <div className='col-12 col-sm-12 col-md-12 col-lg-11 col-xl-11'>
-              <div className=' row justify-content-center'>
-                <div className='mb-4 col-10 col-sm-10 col-md-3 col-lg-3 col-xl-3'>
-                  <CampoTextoNormalComponent id="cedula" name="cedula" campoObligatorio="1" labelCampo="Cedula" clasesStyleLabel="text-white" clasesStyleCampo="form-control" placeholder="CEDULA"/>
-                </div>
-                <div className='mb-4 col-10 col-sm-10 col-md-3 col-lg-3 col-xl-3'>
-                  <CampoTextoNormalComponent id="nombres" name="nombres" campoObligatorio="1" labelCampo="Nombres" clasesStyleLabel="text-white" clasesStyleCampo="form-control" placeholder="NOMBRE"/>
-                </div>
-                <div className='mb-4 col-10 col-sm-10 col-md-3 col-lg-3 col-xl-3'>
-                  <CampoTextoNormalComponent id="apellidos" name="apellidos" campoObligatorio="1" labelCampo="Apellidos" clasesStyleLabel="text-white" clasesStyleCampo="form-control" placeholder="APELLIDOS"/>
-                </div>
-              </div>
-              <div className=' row justify-content-center'>
-                <div className='mb-4 col-10 col-sm-10 col-md-3 col-lg-3 col-xl-3'>
-                  <CampoTextoNormalComponent id="correo" name="correo" campoObligatorio="1" labelCampo="Correo" clasesStyleLabel="text-white" clasesStyleCampo="form-control" placeholder="ejemplo@gmail.com"/>
-                </div>
-                <div className='mb-4 col-10 col-sm-10 col-md-3 col-lg-3 col-xl-3'>
-                  <CampoTextoNormalComponent id="telefono_movil" name="telefono_movil" campoObligatorio="1" labelCampo="Telefono Movil" clasesStyleLabel="text-white" clasesStyleCampo="form-control" placeholder="00000000000"/>
-                </div>
-                <div className='mb-4 col-10 col-sm-10 col-md-3 col-lg-3 col-xl-3'>
-                  <CampoTextoNormalComponent id="telefono_fijo" name="telefono_fijo" campoObligatorio="1" labelCampo="Telefono Fijo" clasesStyleLabel="text-white" clasesStyleCampo="form-control" placeholder="00000000000"/>
-                </div>
-              </div>
-              <div className=' row justify-content-center'>                
-                <div className='mb-4 col-10 col-sm-10 col-md-9 col-lg-9 col-xl-9'>
-                  <CampoTextAreaNormalComponent id="direccion" name="direccion" campoObligatorio="1" labelCampo="Dirección" clasesStyleLabel="text-white" clasesStyleCampo="form-control"/>
-                </div>
-              </div>
-              <div className=' row justify-content-center mb-4'>                
-                <div className='col-10 col-sm-10 col-md-9 col-lg-9 col-xl-9'>
-                  <h2 className=' text-white'>Foto</h2>
-                </div>
-              </div>
-              <div className=' row justify-content-center mb-5'>  
-                <input type="file" id='campoFoto' name='campoFoto' onChange={cargarFoto} hidden/>
-                <div className='col-auto'>
-                  <label htmlFor='campoFoto' className='imagen_label'>
-                    <span  className='imagen'>Seleccionar Imagen</span>
-                  </label>
-                </div>
-              </div>
-              <div className=' row justify-content-center mb-4 '>                
-                <div className='col-auto'>
-                  {operacion==="registrar" && 
-                    <button onClick={mostrarModal} className="btn btn-primary">Registra</button>
-                  }
-                  {operacion==="actualizar" && 
-                    <button onClick={mostrarModal} className="btn btn-warning">Actualizar</button>
-                  }
-                </div>
-                <div className='col-auto'>
-                  <Link href="/cliente" className="btn btn-danger ">Cancelar</Link>
-                </div>
-              </div>
-            </div>
-        </form>
-      </div>
-    </section>
-  )
-  return(
-  <>
-    <Head>
-    <title>WISP | Modulo Cliente Formulario</title>
-    <meta name="description" content="ss" />
-    <link rel="icon" href="/favicon.ico" />
-    </Head>
-    <DashboardComponent contenidoDasborad={contenido}/>
-  </>
-  )
+        </section>
+    )
+    return (
+        <>
+            <Head>
+                <title>WISP | Modulo Cliente Formulario</title>
+                <meta name="description" content="ss" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <DashboardComponent contenidoDasborad={contenido} />
+        </>
+    )
 }
--- frontend/pages/plan/[operacion].js
+++ frontend/pages/plan/[operacion].js
@@ -6,41 +6,162 @@ import AlertComponent from '../../components/alertComponent'
 // component form
 import CampoTextoNormalComponent from "../../components/campoTextoNormalComponent"
 import CampoTextAreaNormalComponent from "../../components/campoTextAreaNormalComponent"
+import CampoSelectdNormalComponent from '../../components/campoSelectNormalComponent'
 import ModalComponent from '../../components/modalComponent'
 // hook
-import {useRouter} from 'next/router'
+import { useRouter } from 'next/router'
+import { useState } from 'react'
+// TODO: agregar data dinamica al campo velocidad usando el hook useState y useEffect
+// TODO: agregar data estatica al campo medida usando el hook useState y useEffect
 
-export default function Formulario(){
+export default function Formulario() {
     const router = useRouter()
-    let {operacion} = router.query
-    const ruta= [
-      {
-        ruta:"/",
-        textLink:"Home"
-      },
-      {
-        ruta:"/Plan",
-        textLink:"Plan"
-      },
-      {
-        textLink:operacion
-      }
+    // const [velocidad,setVelocidad] = useState(0)
+    let { operacion } = router.query
+    const ruta = [
+        {
+            ruta: "/",
+            textLink: "Home"
+        },
+        {
+            ruta: "/plan",
+            textLink: "Plan"
+        },
+        {
+            textLink: operacion
+        }
     ]
 
-    let contenido=(
+    function modalToggle(boton) {
+        let { target } = boton
+        modalTaggleId(target.getAttribute("data-id-modal"))
+    }
+
+    function modalTaggleId(id) {
+        let modal = document.getElementById(id)
+        if (modal.classList.contains("show")) {
+            modal.classList.remove("show")
+            modal.style.display = "none"
+            modal.removeAttribute("role")
+            modal.removeAttribute("aria-modal")
+            modal.setAttribute("aria-hidden", "true")
+        }
+        else {
+            modal.classList.add("show")
+            modal.style.display = "block"
+            modal.setAttribute("role", "dialog")
+            modal.setAttribute("aria-modal", "true")
+            modal.removeAttribute("aria-hidden")
+        }
+    }
+
+    function cancelarOnSubmit(e) {
+        e.preventDefault()
+    }
+
+    function mostrarModal() {
+        modalTaggleId("modal")
+        let modalLabel = document.getElementById("modalLabel")
+        let mensajeModal = document.getElementById("mensajeModal")
+        if (operacion === "registrar") {
+            modalLabel.textContent = "Registrar"
+            mensajeModal.textContent = "Esta seguro que desea registrar"
+        }
+        else {
+            modalLabel.textContent = "Actualizar"
+            mensajeModal.textContent = "Esta seguro que desea actualizar"
+        }
+    }
+
+    function enviarDatos() {
+        alert("ejecutando " + operacion)
+    }
+
+    let contenido = (
         <section className='min-vh-100 modulo-plan-formulario pb-5'>
-            <h1> vista formulario del modulo plan </h1>
+            <BreadcrumbComponent ruta={ruta} />
+            <div className='d-flex mb-5 px-5'>
+                <Link href="/cliente" className="btn btn-primary">Regresar</Link>
+            </div>
+            <h1 className=' text-white text-center mb-2'>Formulario Plan</h1>
+            <div className='text-white text-center mb-4'>los campos con <span className='text-danger'>(*)</span> son obligatorios</div>
+            <div className=' container-fluid'>
+                <div className=' row justify-content-center'>
+                    <div id="columnaAlerta" className=' col-10 col-sm-10 col-md-9 col-lg-9 col-xl-8'>
+                        {/* zona aleta */}
+                    </div>
+                </div>
+            </div>
+            <ModalComponent id="modal">
+                <div className="modal-header">
+                    <h1 className="modal-title fs-5" id="modalLabel">---</h1>
+                    <button type="button" className="btn-close" aria-label="Close" data-id-modal="modal" onClick={modalToggle}></button>
+                </div>
+                <div className="modal-body">
+                    <h5 id='mensajeModal'></h5>
+                </div>
+                <div className="modal-footer">
+                    <button type="button" className="btn btn-secondary" data-id-modal="modal" onClick={modalToggle}>Cerrar</button>
+                    {operacion === "registrar" &&
+                        <button onClick={enviarDatos} className="btn btn-primary">guardar</button>
+                    }
+                    {operacion === "actualizar" &&
+                        <button onClick={enviarDatos} className="btn btn-warning">guardar</button>
+                    }
+                </div>
+            </ModalComponent>
+            <div className=' container-fluid'>
+                <form id="formularioPlan" onSubmit={cancelarOnSubmit} className=' row justify-content-center'>
+                    <div className='col-12 col-sm-12 col-md-12 col-lg-11 col-xl-11'>
+                        <div className=' row justify-content-center'>
+                            <div className='mb-4 col-10 col-sm-10 col-md-3 col-lg-3 col-xl-3'>
+                                <CampoTextoNormalComponent id="nombre" name="nombre" campoObligatorio="1" labelCampo="Nombre" clasesStyleLabel="text-white" clasesStyleCampo="form-control" placeholder="NOMBRE" />
+                            </div>
+                            <div className='mb-4 col-10 col-sm-10 col-md-3 col-lg-3 col-xl-3'>
+                                <CampoSelectdNormalComponent opcionesSelect={[]} id="velocidad" name="velocidad" campoObligatorio="1" labelCampo="Velocidad" clasesStyleLabel="text-white" clasesStyle="form-control" />
+                            </div>
+                            <div className='mb-4 col-10 col-sm-10 col-md-3 col-lg-3 col-xl-3'>
+                                <CampoSelectdNormalComponent opcionesSelect={[]} id="medida_velocidad" name="medida_velocidad" campoObligatorio="1" labelCampo="Medida" clasesStyleLabel="text-white" clasesStyle="form-control" />
+                            </div>
+                        </div>
+                        <div className=' row justify-content-center'>                            
+                            <div className='mb-4 col-10 col-sm-10 col-md-3 col-lg-3 col-xl-3'>
+                                <CampoTextoNormalComponent id="precio" name="precio" campoObligatorio="1" labelCampo="Precio" clasesStyleLabel="text-white" clasesStyleCampo="form-control" placeholder="PRECIO" />
+                            </div>
+                            <div className='mb-4 col-10 col-sm-10 col-md-3 col-lg-3 col-xl-3 offset-10 offset-sm-10 offset-md-3 offset-lg-3 offset-xl-3'></div>
+                        </div>
+                        <div className=' row justify-content-center'>
+                            <div className='mb-4 col-10 col-sm-10 col-md-9 col-lg-9 col-xl-9'>
+                                <CampoTextAreaNormalComponent id="descripcion" name="descripcion" campoObligatorio="1" labelCampo="Descripción" clasesStyleLabel="text-white" clasesStyleCampo="form-control" />
+                            </div>
+                        </div>
+                        <div className=' row justify-content-center mb-4 '>
+                            <div className='col-auto'>
+                                {operacion === "registrar" &&
+                                    <button onClick={mostrarModal} className="btn btn-primary">Registra</button>
+                                }
+                                {operacion === "actualizar" &&
+                                    <button onClick={mostrarModal} className="btn btn-warning">Actualizar</button>
+                                }
+                            </div>
+                            <div className='col-auto'>
+                                <Link href="/plan" className="btn btn-danger ">Cancelar</Link>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            </div>
         </section>
     )
 
-    return(
+    return (
         <>
-        <Head>
-          <title>WISP | Modulo Plan Formulario</title>
-          <meta name="description" content="ss" />
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
-        <DashboardComponent contenidoDasborad={contenido}/>
-      </>
+            <Head>
+                <title>WISP | Modulo Plan Formulario</title>
+                <meta name="description" content="ss" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <DashboardComponent contenidoDasborad={contenido} />
+        </>
     )
 }
